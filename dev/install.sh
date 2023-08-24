1c60b6bf3297a4c7917d66d119b10f5ade42414d
peng <peng.cheng@acyclic.ai>
2023-08-24 17:07:52 -0400

    gnome upgrade to 44

--- dev/install.sh
+++ dev/install.sh
@@ -1,4 +1,4 @@
-flatpak install org.gnome.Sdk/x86_64/42
-flatpak install org.gnome.Platform/x86_64/42
+flatpak install org.gnome.Sdk/x86_64/44
+flatpak install org.gnome.Platform/x86_64/44
 
 flatpak-builder --user --install --force-clean build-dir com.vmware.HorizonClient.yaml
